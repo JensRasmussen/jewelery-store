b57ed5d89b2265e0aeeb9f97d99e9edd67821067
Moditha Hewasinghage <mhewasinghage@liveintent.com>
2023-04-08 18:49:18 +0200

    Add bootstrap template

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const path = require('path')
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: path.resolve(__dirname, 'src'),
   base: '/jewelery-store/',
   plugins: [react()],
+  server: {
+    port: 8080,
+    hot: true
+  },
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
 })
